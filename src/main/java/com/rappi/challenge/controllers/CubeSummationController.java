ac40f6a703e5b7c07e064f2a2013747a00c99515
Anibal A. Lovaglio <alelovaglio@gmail.com>
2018-01-26 13:22:11 -0300

    Flujo de pantallas: form de input y pantalla de resultados (falta armarla a partir de los resultados)

--- src/main/java/com/rappi/challenge/controllers/CubeSummationController.java
+++ src/main/java/com/rappi/challenge/controllers/CubeSummationController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CubeSummationController {
-
+	
 	@RequestMapping(value = "/cube", method = RequestMethod.GET)
-	public String greeting() {
+	public String get() {
 		return "cube-in";
+	}
+
+	@RequestMapping(value = "/cube", method = RequestMethod.POST)
+	public String post() {
+		System.out.println("Hola!!!");
+		return "cube-out";
 	}
 
 }
